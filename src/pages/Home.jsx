fa0355c219d4d1c302063ba65e9ae8949f312af8
motain1 <jacksontfry@gmail.com>
2022-10-08 16:35:02 +1100

    Search Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Avatar } from "@mui/material";
+import Search from "../components/Search";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>this is come pahe</h1>
       <div className="home__header">
         <div className="home__headerLeft">
-          <Link to="/about">about</Link>
+          <Link to="/about">About</Link>
           <Link to="/store">Store</Link>
         </div>
         <div className="home__headerRight">
           <Link to="/gmail">Gmail</Link>
           <Link to="/images">Images</Link>
-          {/* icon */}
-          {/* avatar */}
+          <AppsIcon />
+          <Avatar />
         </div>
       </div>
-      <div className="home__body"></div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+          alt=""
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
     </div>
   );
 };
